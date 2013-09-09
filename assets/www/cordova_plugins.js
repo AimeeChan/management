4096caea869f6dd023be9a81cbdaef3b16268625
AimeeChan <chenxianlan@gmail.com>
2013-09-09 16:54:31 +0800

    '20130909'

---
+++ assets/www/cordova_plugins.js
@@ -0,0 +1,39 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins\\org.apache.cordova.core.device\\www\\device.js",
+        "id": "org.apache.cordova.core.device.device",
+        "clobbers": [
+            "device"
+        ]
+    },
+    {
+        "file": "plugins\\org.apache.cordova.core.camera\\www\\CameraConstants.js",
+        "id": "org.apache.cordova.core.camera.Camera",
+        "clobbers": [
+            "Camera"
+        ]
+    },
+    {
+        "file": "plugins\\org.apache.cordova.core.camera\\www\\CameraPopoverOptions.js",
+        "id": "org.apache.cordova.core.camera.CameraPopoverOptions",
+        "clobbers": [
+            "CameraPopoverOptions"
+        ]
+    },
+    {
+        "file": "plugins\\org.apache.cordova.core.camera\\www\\Camera.js",
+        "id": "org.apache.cordova.core.camera.camera",
+        "clobbers": [
+            "navigator.camera"
+        ]
+    },
+    {
+        "file": "plugins\\org.apache.cordova.core.camera\\www\\CameraPopoverHandle.js",
+        "id": "org.apache.cordova.core.camera.CameraPopoverHandle",
+        "clobbers": [
+            "CameraPopoverHandle"
+        ]
+    }
+]
+});
